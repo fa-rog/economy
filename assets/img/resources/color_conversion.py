86aa18af37e52406f0f8cb862759cfe88ad8f039
fa-rog <farog.mc@gmail.com>
2023-06-11 21:14:08 +0200

    Simplify script for color conversion of resource images

--- assets/img/resources/color_conversion.py
+++ assets/img/resources/color_conversion.py
@@ -1,6 +1,7 @@
 """Simple script for generating an image of a resource in a specified color
 """
 
+from itertools import product
 from PIL import Image
 
 resource = input('Enter resource name: ').lower()
@@ -8,12 +9,10 @@ image = Image.open(f'{resource}_white.png')
 hex = input('Enter new color in hexadecimal format: ').lstrip('#') + 'ff'
 color = tuple(int(hex[i : i + 2], 16) for i in range(0, 8, 2))
 
-for x in range(image.size[0]):
-    for y in range(image.size[1]):
-        pixel = image.getpixel((x, y))
-        if pixel[3] != 0:
-            new_color = tuple(int(color[i] * pixel[i] / 255) for i in range(4))
-            image.putpixel((x, y), new_color)
+for x, y in product(range(image.size[0]), range(image.size[1])):
+    pixel = image.getpixel((x, y))
+    new_color = tuple(int(color[i] * pixel[i] / 255) for i in range(4))
+    image.putpixel((x, y), new_color)
 
 image.save(f'{resource}_new.png')
 print('Image saved')
